7f6057e3a84323eaf52a8a73c215e2e7ffea304a
Dikdik Alfiana R <linuxer.indonesia@gmail.com>
2018-06-05 13:05:25 +0700

    upload uas_prak_pi

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,25 @@
+@extends('includes.head')
+
+@section('title','Create Category')
+
+@section('content')
+
+		<div class="container">
+				<div class="text-center">><h1>Halaman Buat Kategori</h1></div>
+				<hr>
+
+				<div class="col-md-8 col-md-offset-2">
+					<div class="form-group">
+						<form action="" method="post"
+						<div class="form-group">
+							<input type="" name="" class="form-control" placeholder="Buat Kategori Baru"...>
+						</div>
+
+						<button type="submit" class="btn btn-success btn-block">Save</button>
+
+					</form>
+				</div>
+			</div>
+		</div>
+
+@endsection
